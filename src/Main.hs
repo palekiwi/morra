34dbdb6847fa818f835cbbcc3aaf2cff87992980
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-12-04 17:46:26 +0800

    write player vs player version

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,13 @@ module Main where
 
 import Data.Char (toUpper, isDigit, digitToInt)
 import System.Random
+import System.Exit (exitSuccess)
 import Control.Monad
 import Control.Monad.Trans.State
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Class
 import Control.Monad.IO.Class
+import System.Console.ANSI (clearScreen)
 
 type Morra = StateT Score (ReaderT Config IO)
 type Score = (Int, Int)
@@ -15,36 +17,63 @@ newtype Config = Config {
                        gameplay :: Gameplay
                      }
 
-scoreRound :: Int -> Score -> Score
-scoreRound total (a,b) =
-  if odd total
-     then (a + 1, b)
-     else (a, b + 1)
+playerInput :: String -> IO (Maybe Int)
+playerInput i = do
+  putStr i
+  c <- getChar
+  _ <- getChar
+  case c of
+    '1' -> return (Just 1)
+    '2' -> return (Just 2)
+    'q' -> return Nothing
+    _   -> do
+      putStrLn "Invalid input."
+      playerInput i
+
+scoreRound :: Int -> Morra ()
+scoreRound total = modify f
+  where f (a,b) = if odd total
+                    then (a + 1, b)
+                    else (a, b + 1)
 
 printRoundResult :: Int -> IO ()
 printRoundResult n = if odd n
-                        then putStrLn "Player Scores"
-                        else putStrLn "Computer Scores"
+                        then putStrLn "Odd Scores"
+                        else putStrLn "Even Scores"
 
 p2p :: Morra ()
-p2p = undefined
+p2p = do
+  total <- liftIO $ do
+    p1 <- playerInput "P1: "
+    clearScreen
+    p2 <- playerInput "P2: "
+    return $ (+) <$> p1 <*> p2
+
+  case total of
+    Just t -> do
+      scoreRound t
+      liftIO $ printRoundResult t
+      p2cpu
+    Nothing -> return ()
 
 p2cpu :: Morra ()
 p2cpu = do
-  liftIO $ putStr "P: "
-  p <- liftIO getChar
-  _ <- liftIO getChar
-  when (isDigit p) $ do
-    let p' = digitToInt p
-    c <- liftIO randomCPU
-    liftIO . putStrLn $ "C: " ++ show c
-    let total = p' + c
-    modify (scoreRound total)
-    liftIO $ printRoundResult total
-    p2cpu
+  total <- liftIO $ do
+    p <- playerInput "P1: "
+    c <- randomCPU
+    putStrLn $ "C: " ++ show c
+    return $ (+c) <$> p
+
+  case total of
+    Just t -> do
+      scoreRound t
+      liftIO $ printRoundResult t
+      p2cpu
+    Nothing -> return ()
 
 morra :: Morra ()
 morra = do
+  liftIO clearScreen
   gp <- lift (asks gameplay)
   case gp of
     P2P -> do
@@ -63,6 +92,7 @@ printScore gp (a,b) = do
                   P2P -> ("Player 1", "Player 2")
                   P2CPU -> ("Player", "Computer")
 
+  liftIO clearScreen
   putStrLn "#####################"
   putStrLn "#### GAME RESULT ####"
   putStrLn $ "# " ++ p1 ++ " score: " ++ show a
@@ -82,16 +112,20 @@ getGameplay = do
   c <- getChar
   _ <- getChar
   case c of
+    'q' -> exitSuccess
     '1' -> return P2P
     '2' -> return P2CPU
     _   -> do
-      putStrLn "Invalid input. Please enter 1 2 or q"
+      putStrLn "Invalid input. Please enter: 1 2 or q"
       getGameplay
 
-main :: IO ()
-main = do
+runMorra :: IO ()
+runMorra = do
   putStrLn "Welcome to Morra game"
   gp <- getGameplay
   (_, score) <- flip runReaderT (Config gp)
            $ runStateT morra (0,0)
   printScore gp score
+
+main :: IO ()
+main = runMorra
